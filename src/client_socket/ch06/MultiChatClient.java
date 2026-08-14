2dfab0dcc7ca69a11b367229d1fbe711fd59d4f8
yuyn2000 <yuyn2000@naver.com>
2026-08-14 17:30:07 +0900

    socket 1 : N

---
+++ src/client_socket/ch06/MultiChatClient.java
@@ -0,0 +1,10 @@
+package client_socket.ch06;
+
+abstract class AbstractClient {
+
+}
+
+
+public class MultiChatClient {
+
+}
---
+++ src/socket/ch05/MultiChatServer.java
@@ -0,0 +1,26 @@
+package socket.ch05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
+
+public class MultiChatServer {
+     private static final int PORT = 5000;
+     private static final Vector<PrintWriter> writer = new Vector<>();
+
+     static class ServerStream extends Thread {
+
+         }
+
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
